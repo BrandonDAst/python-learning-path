eb141d57ffb8ab2b5d161b34e02df4a333934fd7
Brandon Diaz <brandon.diaz.as@gmail.com>
2021-10-20 11:40:08 -0500

    Conditions

---
+++ conditions.py
@@ -0,0 +1,9 @@
+mayoria_edad = 18
+edad = int(input("Escribe tu edad: "))
+if edad >= mayoria_edad:
+    print("Es mayor de edad.")
+elif edad == 1:
+    print("Es recien nacido")
+else:
+    pass
+    # print("Eres menor de edad")
