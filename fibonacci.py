95d6940236e28c158c3a1a86c71b8ca1edad2a9e
Brandon Diaz <brandon.diaz.as@gmail.com>
2021-10-20 17:37:07 -0500

    Fibonacci

---
+++ fibonacci.py
@@ -0,0 +1,26 @@
+def printFibonacci(postition):
+    list = []
+    num1 = 0
+    num2 = 1
+    num3 = 0
+
+    list.append(num1)
+    list.append(num2)
+
+    for i in range(postition):
+        num3 = num1 + num2
+        list.append(num3)
+
+        num1 = num2
+        num2 = num3
+
+    print(list)
+
+
+def run():
+    postition = int(input("Position: "))
+    printFibonacci(postition)
+
+
+if __name__ == '__main__':
+    run()
